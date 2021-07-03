6f6c9eec3fc2a8ba0f139e0c1d5b7ee212697900
granwyntan <granwyn_tan@s2019.ssts.edu.sg>
2021-07-03 12:21:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from tkinter import *
 from tkinter import filedialog
 
+
 def openFile():
-    filepath = filedialog.askopenfilename(initialdir="C:\\Users\\Cakow\\PycharmProjects\\Main",
+    filepath = filedialog.askopenfilename(initialdir="",
                                           title="Open Acquired Spectral Data File",
-                                          filetypes= (("Text Files","*.txt"), ("ASCII Files","*.asc"),
-                                          ("All Files","*.*")))
-    file = open(filepath,'r')
+                                          filetypes=(("Text Files", "*.txt"), ("ASCII Files", "*.asc"),
+                                                     ("All Files", "*.*")))
+    file = open(filepath, 'r')
     print(file.read())
     file.close()
 
-window = Tk()
+
+root = Tk()
 button = Button(text="Open", command=openFile)
 button.pack()
-window.mainloop()
+root.mainloop()
